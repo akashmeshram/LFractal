71352f6221590b44bd34b88c1a8cdd77ccacd3fa
Akash Meshram <6388495akash@gmail.com>
2021-01-30 12:01:12 +0530

    Removed not-required types

--- src/store/types.js
+++ src/store/types.js
@@ -1,18 +1,3 @@
-// Controls
-export const ADD_FACTOR = 'ADD_FACTOR';
-export const ADD_AXIOM = 'ADD_AXIOM';
-export const ADD_RULE1 = 'ADD_RULE1';
-export const ADD_RULE2 = 'ADD_RULE2';
-export const ADD_STARTX = 'ADD_STARTX';
-export const ADD_STARTY = 'ADD_STARTY';
-export const ADD_ITERATION = 'ADD_ITERATION';
-export const ADD_LENGTH = 'ADD_LENGTH';
-export const ADD_ANGLE = 'ADD_ANGLE';
-export const ADD_TICKNESS = 'ADD_TICKNESS';
+export const ADD_CONTROLE = 'ADD_CONTROLE';
 export const ADD_CONTROLE_FAILED = 'ADD_CONTROLE_FAILED';
 export const ADD_ALL_CONTROLS = 'ADD_ALL_CONTROLS';
-
-// Controls
-export const ADD_PATTERNS_STARTED = 'ADD_PATTERNS_STARTED';
-export const ADD_PATTERNS_SUCCESS = 'ADD_PATTERNS_SUCCESS';
-export const ADD_PATTERNS_FAILED = 'ADD_PATTERNS_FAILED';
